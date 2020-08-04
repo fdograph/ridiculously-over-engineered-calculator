1a559c97997e41c94270c1d301282d40d1ecabdc
Fernando Silva <fdograph@gmail.com>
2020-08-04 21:08:38 +0200

    Updates

---
+++ precache-manifest.da831d8f64e3ccc75d1c89f2ee753574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b687ba9510a7a6be21dcb42c202026d",
+    "url": "/ridiculously-over-engineered-calculator/index.html"
+  },
+  {
+    "revision": "02479aeb6b357c8c1274",
+    "url": "/ridiculously-over-engineered-calculator/static/css/main.4ab342a9.chunk.css"
+  },
+  {
+    "revision": "5cd7e005c21f77c29582",
+    "url": "/ridiculously-over-engineered-calculator/static/js/2.220429d5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ridiculously-over-engineered-calculator/static/js/2.220429d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02479aeb6b357c8c1274",
+    "url": "/ridiculously-over-engineered-calculator/static/js/main.2ed4e398.chunk.js"
+  },
+  {
+    "revision": "698e33752dd6092482d7",
+    "url": "/ridiculously-over-engineered-calculator/static/js/runtime-main.9405a397.js"
+  }
+]);
